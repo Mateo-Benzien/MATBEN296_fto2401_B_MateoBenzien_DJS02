84e7cca849ce4f03c7cfcea2bc246c5215d5113f
Mateo Benzien <156769785+Mateo-Benzien@users.noreply.github.com>
2024-05-09 21:03:04 +0200

    Update.1 scripts.js

--- scripts.js
+++ scripts.js
@@ -5,5 +5,35 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  // Scenario: Validation when values are missing
+  if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
+
+  // Scenario: An invalid division should log an error in the console
+  if (divider == 0 || isNaN(dividend) || isNaN(divider)) {
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+    console.error("Error: Invalid division");
+    return;
+  }
+
+  // Scenario: Providing anything that is not a number should crash the program
+  if (isNaN(dividend) || isNaN(divider)) {
+    document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page</h1>";
+    console.error("Error: Non-numeric input detected");
+    return;
+  }
+
+  // Perform division
+  const divisionResult = dividend / divider;
+
+  // Scenario: Dividing numbers result in a whole number
+  if (divisionResult % 1 !== 0) {
+    result.innerText = divisionResult;
+  } else {
+    // Scenario: Dividing numbers result in a decimal number
+    result.innerText = Math.trunc(divisionResult);
+  }
 });
